0d4db4654416096d0774fbc26d9f896f9d770adf
Oleh Mytlovych <oleh.mytlovych@gmail.com>
2021-02-26 21:02:14 +0200

    firebase dataBase setup and transfer of app from local jsons to firebase endpoints

---
+++ ace/src/app/dataUrls.ts
@@ -0,0 +1,6 @@
+export const baseDataUrl = 'https://acey-6f81c-default-rtdb.firebaseio.com';
+export const prosSuffixUrl = '/professionals.json';
+export const catsSuffixUrl = './categories.json';
+export const jobsSuffixUrl = './jobs.json';
+export const partnsSuffixUrl = './partners.json';
+export const custsSuffixUrl = './customers.json';
--- ace/src/app/sharedServices/professional/professional.service.ts
+++ ace/src/app/sharedServices/professional/professional.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Professional, ProCategory } from '../../interfaces/professional';
 import { map } from 'rxjs/operators';
+
+import { Professional, ProCategory } from '../../interfaces/professional';
+import { baseDataUrl, prosSuffixUrl } from './../../dataUrls';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfessionalService {
-  public configUrl = 'assets/mocks/professionals.json';
 
   constructor(private http: HttpClient) { }
 
   public getProfessionals(category: string) {
-    return this.http.get<Professional[]>(this.configUrl).pipe(
+    return this.http.get<Professional[]>(`${baseDataUrl}${prosSuffixUrl}`).pipe(
       map((pros) => {
         if (!category) { return pros; }
 
